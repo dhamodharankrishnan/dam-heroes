79b3c4a48c3528f8baa6c06a4f90777145ba3e50
Dhamodharan Krishnan <dhamodharan.krishnan@walgreens.com>
2019-07-01 00:54:52 -0400

    Changes while doing demo

--- src/app/hero-birthday.ts
+++ src/app/hero-birthday.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero-birthday',
-  template: `<p>The hero's birthday is {{ birthday | date:"MM/dd/yyyy" }}</p>`
+  template: `<p>The hero's birthday is {{ birthday | date:"yyyy-MM-dd" }}</p>`
 })
 export class HeroBirthdayComponent {
     birthday = '03/18/2000';
